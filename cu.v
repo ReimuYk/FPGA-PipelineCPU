9c87c2ee5dc429b04c7d5af0eaa8507f1f8ecd55
ReimuYk <513639514@qq.com>
2018-12-11 19:33:49 +0800

    update cu

--- cu.v
+++ cu.v
@@ -8,42 +8,57 @@ module pipe_cu(op, func, rs, rt, ern, mrn, rsrtequ, ewreg, em2reg, mwreg, mm2reg
 	output reg [1:0] fwda, fwdb;
 	output     [3:0] aluc;
 
-	wire r_type = op == 6'b000000;
-	wire i_add = r_type & func == 6'b100000;
-	wire i_sub = r_type & func == 6'b100010;
-	wire i_and = r_type & func == 6'b100100;
-	wire i_or  = r_type & func == 6'b100101;
-	wire i_xor = r_type & func == 6'b100110;
-	wire i_sll = r_type & func == 6'b000000;
-	wire i_srl = r_type & func == 6'b000010;
-	wire i_sra = r_type & func == 6'b000011;
-	wire i_jr  = r_type & func == 6'b001000;
-	wire i_addi = op == 6'b001000;
-	wire i_andi = op == 6'b001100;
-	wire i_ori  = op == 6'b001101;
-	wire i_xori = op == 6'b001110;
-	wire i_lw   = op == 6'b100011;
-	wire i_sw   = op == 6'b101011;
-	wire i_beq  = op == 6'b000100;
-	wire i_bne  = op == 6'b000101;
-	wire i_lui  = op == 6'b001111;
-	wire i_j    = op == 6'b000010;
-	wire i_jal  = op == 6'b000011;
+	 wire r_type = ~|op;
+   wire i_add = r_type & func[5] & ~func[4] & ~func[3] &
+                ~func[2] & ~func[1] & ~func[0];          //100000
+   wire i_sub = r_type & func[5] & ~func[4] & ~func[3] &
+                ~func[2] &  func[1] & ~func[0];          //100010
+      
+   //  please complete the deleted code.
+   
+   wire i_and = r_type & func[5] & ~func[4] & ~func[3] &
+                func[2] & ~func[1] & ~func[0];          //100100
+   wire i_or  = r_type & func[5] & ~func[4] & ~func[3] &
+                func[2] & ~func[1] & func[0];          //100101
+
+   wire i_xor = r_type & func[5] & ~func[4] & ~func[3] &
+                func[2] & func[1] & ~func[0];          //100110
+   wire i_sll = r_type & ~func[5] & ~func[4] & ~func[3] &
+                ~func[2] & ~func[1] & ~func[0];          //000000
+   wire i_srl = r_type & ~func[5] & ~func[4] & ~func[3] &
+                ~func[2] & func[1] & ~func[0];          //000010
+   wire i_sra = r_type & ~func[5] & ~func[4] & ~func[3] &
+                ~func[2] & func[1] & func[0];          //000011
+   wire i_jr  = r_type & ~func[5] & ~func[4] & func[3] &
+                ~func[2] & ~func[1] & ~func[0];          //001000
+                
+   wire i_addi = ~op[5] & ~op[4] &  op[3] & ~op[2] & ~op[1] & ~op[0]; //001000
+   wire i_andi = ~op[5] & ~op[4] &  op[3] &  op[2] & ~op[1] & ~op[0]; //001100
+   
+   wire i_ori  = ~op[5] & ~op[4] &  op[3] &  op[2] & ~op[1] &  op[0]; //001101
+   wire i_xori = ~op[5] & ~op[4] &  op[3] &  op[2] &  op[1] & ~op[0]; //001110
+   wire i_lw   =  op[5] & ~op[4] & ~op[3] & ~op[2] &  op[1] &  op[0]; //100011
+   wire i_sw   =  op[5] & ~op[4] &  op[3] & ~op[2] &  op[1] &  op[0]; //101011
+   wire i_beq  = ~op[5] & ~op[4] & ~op[3] &  op[2] & ~op[1] & ~op[0]; //000100
+   wire i_bne  = ~op[5] & ~op[4] & ~op[3] &  op[2] & ~op[1] &  op[0]; //000101
+   wire i_lui  = ~op[5] & ~op[4] &  op[3] &  op[2] &  op[1] &  op[0]; //001111
+   wire i_j    = ~op[5] & ~op[4] & ~op[3] & ~op[2] &  op[1] & ~op[0]; //000010
+   wire i_jal  = ~op[5] & ~op[4] & ~op[3] & ~op[2] &  op[1] &  op[0]; //000011
 
 	// Determine which instructions use rs/rt.
-	wire use_rs = i_add | i_sub | i_and | i_or | i_xor | i_jr | i_addi | i_andi | i_ori | i_xori
+	wire urs = i_add | i_sub | i_and | i_or | i_xor | i_jr | i_addi | i_andi | i_ori | i_xori
 		| i_lw | i_sw | i_beq | i_bne;
-	wire use_rt = i_add | i_sub | i_and | i_or | i_xor | i_sll | i_srl | i_sra | i_sw | i_beq | i_bne;
+	wire urt = i_add | i_sub | i_and | i_or | i_xor | i_sll | i_srl | i_sra | i_sw | i_beq | i_bne;
 
-	wire load_use_hazard = ewreg & em2reg & (ern != 0) & ((use_rs & (ern == rs)) | (use_rt & (ern == rt)));
+	wire LUhazard = ewreg & em2reg & (ern != 0) & ((urs & (ern == rs)) | (urt & (ern == rt)));
 
 	// When load/use hazard happens, stall F and D registers (stall PC),
 	// and generate a bubble to E register (by forbidding writing registers and memory).
-	assign wpcir = ~load_use_hazard;
+	assign wpcir = ~LUhazard;
 	assign wreg = (i_add | i_sub | i_and | i_or | i_xor | i_sll | i_srl | i_sra
-		| i_addi | i_andi | i_ori | i_xori | i_lw | i_lui | i_jal) & ~load_use_hazard;
+		| i_addi | i_andi | i_ori | i_xori | i_lw | i_lui | i_jal) & ~LUhazard;
 	assign m2reg = i_lw;
-	assign wmem = i_sw & ~load_use_hazard;
+	assign wmem = i_sw & ~LUhazard;
 	assign jal = i_jal;
 	assign aluimm = i_addi | i_andi | i_ori | i_xori | i_lw | i_sw | i_lui;
 	assign shift = i_sll | i_srl | i_sra;
@@ -63,23 +78,23 @@ module pipe_cu(op, func, rs, rt, ern, mrn, rsrtequ, ewreg, em2reg, mwreg, mm2reg
 	// Also, we should not forward r0.
 	always @(*) begin
 		if (ewreg & ~em2reg & (ern != 0) & (ern == rs))
-			fwda = 2'b01; // Forward from ealu.
+			fwda = 2'b01; // ealu.
 		else if (mwreg & ~mm2reg & (mrn != 0) & (mrn == rs))
-			fwda = 2'b10; // Forward from malu.
+			fwda = 2'b10; // malu.
 		else if (mwreg & mm2reg & (mrn != 0) & (mrn == rs))
-			fwda = 2'b11; // Forward from mmo.
+			fwda = 2'b11; // mmo.
 		else
-			fwda = 2'b00; // Do not forward.
+			fwda = 2'b00; 
 	end
 
 	always @(*) begin
 		if (ewreg & ~em2reg & (ern != 0) & (ern == rt))
-			fwdb = 2'b01; // Forward from ealu.
+			fwdb = 2'b01; // ealu.
 		else if (mwreg & ~mm2reg & (mrn != 0) & (mrn == rt))
-			fwdb = 2'b10; // Forward from malu.
+			fwdb = 2'b10; // malu.
 		else if (mwreg & mm2reg & (mrn != 0) & (mrn == rt))
-			fwdb = 2'b11; // Forward from mmo.
+			fwdb = 2'b11; // mmo.
 		else
-			fwdb = 2'b00; // Do not forward.
+			fwdb = 2'b00; 
 	end
 endmodule
