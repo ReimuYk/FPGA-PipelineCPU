7919dd23a8a064614eb2103c7ff00bb92ebb54c6
ReimuYk <513639514@qq.com>
2018-12-12 20:30:08 +0800

    remove useless files

--- stage_W.v
+++
@@ -1,7 +0,0 @@
-module pipe_W_stage(walu, wmo, wm2reg, wdi);
-	input         wm2reg;
-	input  [31:0] walu, wmo;
-	output [31:0] wdi;
-
-	mux2x32 select_write_data(walu, wmo, wm2reg, wdi);
-endmodule
